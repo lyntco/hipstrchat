f61f3764ecbcc1b61a119b18427d803ad9d45376
lyntco <strawww@gmail.com>
2014-12-02 13:12:56 +1100

    All the small things

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
   def index
     messages = Message.where(room_id: params[:room_id])
     render :json => messages
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,5 @@
 class RoomsController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
   def index
     rooms = Room.all
     @messages = Message.all
