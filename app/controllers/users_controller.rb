c9f73fe40fe9c4ab5267eea9b94cfa32443dd612
lyntco <strawww@gmail.com>
2014-12-02 13:24:25 +1100

    Create users route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   # skip_before_action :verify_authenticity_token
+  def index
+    users = User.all
+    render :json => users
+  end
+
   def create
     user = User.new(user_params)
     if user.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,23 @@
 Rails.application.routes.draw do
   root 'rooms#index'
 
-  resources :rooms do
-    resources :messages
+  resources :rooms, :only => [:index, :create, :show, :update] do
+    resources :messages, :only => [:create]
   end
 
-  get '/users' => 'users#index'
-  post '/users/create'
-  patch '/users/update'
-  put '/users/update'
+  resources :users, :only => [:index, :create, :update]
 
 end
 
-
-#            Prefix Verb   URI Pattern                                 Controller#Action
-#              root GET    /                                           rooms#index
-#     room_messages GET    /rooms/:room_id/messages(.:format)          messages#index
-#                   POST   /rooms/:room_id/messages(.:format)          messages#create
-#  new_room_message GET    /rooms/:room_id/messages/new(.:format)      messages#new
-# edit_room_message GET    /rooms/:room_id/messages/:id/edit(.:format) messages#edit
-#      room_message GET    /rooms/:room_id/messages/:id(.:format)      messages#show
-#                   PATCH  /rooms/:room_id/messages/:id(.:format)      messages#update
-#                   PUT    /rooms/:room_id/messages/:id(.:format)      messages#update
-#                   DELETE /rooms/:room_id/messages/:id(.:format)      messages#destroy
-#             rooms GET    /rooms(.:format)                            rooms#index
-#                   POST   /rooms(.:format)                            rooms#create
-#          new_room GET    /rooms/new(.:format)                        rooms#new
-#         edit_room GET    /rooms/:id/edit(.:format)                   rooms#edit
-#              room GET    /rooms/:id(.:format)                        rooms#show
-#                   PATCH  /rooms/:id(.:format)                        rooms#update
-#                   PUT    /rooms/:id(.:format)                        rooms#update
-#                   DELETE /rooms/:id(.:format)                        rooms#destroy
-#             users GET    /users(.:format)                            users#index
-#      users_create POST   /users/create(.:format)                     users#create
-#      users_update PATCH  /users/update(.:format)                     users#update
-#                   PUT    /users/update(.:format)                     users#update
+#        Prefix Verb  URI Pattern                        Controller#Action
+#          root GET   /                                  rooms#index
+# room_messages POST  /rooms/:room_id/messages(.:format) messages#create
+#         rooms GET   /rooms(.:format)                   rooms#index
+#               POST  /rooms(.:format)                   rooms#create
+#          room GET   /rooms/:id(.:format)               rooms#show
+#               PATCH /rooms/:id(.:format)               rooms#update
+#               PUT   /rooms/:id(.:format)               rooms#update
+#         users GET   /users(.:format)                   users#index
+#               POST  /users(.:format)                   users#create
+#          user PATCH /users/:id(.:format)               users#update
+#               PUT   /users/:id(.:format)               users#update
